2e5b911e31ff823df20262e3f2677f2f131a93b4
Dave Trost <davetrost@gmail.com>
2019-11-18 13:46:07 -0800

    set up material UI and Roboto fonts

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
+import 'normalize.css';
+import { Provider } from 'react-redux';
+import store from './store';
+import './styles/fonts.css';
 
 render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
