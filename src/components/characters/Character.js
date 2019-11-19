9073089a81d8480c686cf7017acf7f9fa1ea8d9c
Dave Trost <davetrost@gmail.com>
2019-11-18 17:39:37 -0800

    refactor & syle cleanup

--- src/components/characters/Character.js
+++ src/components/characters/Character.js
@@ -1,28 +1,16 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-// import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { Card, CardMedia, CardHeader } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import classes from './Character.css';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles(() => ({
   card: {
     maxWidth: 345,
   },
   media: {
     height: 0,
-    paddingTop: '56.25%', // 16:9
-  },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
+    paddingTop: '80%',
   },
 }));
 
--- src/components/characters/Characters.js
+++ src/components/characters/Characters.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-// import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { Container, Grid } from '@material-ui/core';
 import { Character } from './Character';
 
 export const Characters = ({ characters, handleClick }) => {
-  const characterEles = characters.map((character, i) => (
-    <Grid key={i} item xs={6} sm={4}>
+  const characterEles = characters.map(character => (
+    <Grid key={character.id} item xs={6} sm={4}>
       <Character 
         character={character}
         handleClick={handleClick}
