05cba23f2946cf4c58344f15216540ed3dfbcb10
Dave Trost <davetrost@gmail.com>
2019-11-19 00:18:56 -0800

    navigation!!!!

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import { CssBaseline } from '@material-ui/core';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Header } from './Header';
 import { Nav } from './Nav';
 import { CharactersContainer } from '../containers/CharactersContainer';
 
 export default function App() {
   return (
-    <main>
+    <Router>
       <CssBaseline />
       <Header />
       <Nav />
-      <CharactersContainer />
-    </main>
+      <Switch>
+        <Route path="/avatar">
+          <CharactersContainer />
+        </Route>
+        <Route path="/simpsons">
+          <h2>simpsons</h2>
+        </Route>
+        <Route path="/">
+          <CharactersContainer />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
+import React, { useState, useEffect } from 'react';
+import { AppBar, Tabs, Tab } from '@material-ui/core';
 import styles from './NavTabs.css';
+import { useHistory, useLocation } from 'react-router-dom';
 
 const LinkTab = props => (
   <Tab
@@ -14,12 +13,27 @@ const LinkTab = props => (
   />
 );
 
+const ROUTER_LOCATIONS = {
+  0: '/avatar',
+  1: '/simpsons',
+  '/avatar': 0,
+  '/simpsons': 1,
+};
+
 export const NavTabs = () => {
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = useState(0);
+  const history = useHistory();
+  const location = useLocation();
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
+    history.push(ROUTER_LOCATIONS[newValue || 0]);
   };
+
+  useEffect(() => {
+    const index = ROUTER_LOCATIONS[location.pathname.toLowerCase()] || 0;
+    setValue(+index);
+  }, [location]);
 
   return (
     <div className={styles.NavTabs}>
@@ -29,8 +43,8 @@ export const NavTabs = () => {
           value={value}
           onChange={handleChange}
         >
-          <LinkTab label="Avatar: The Last Airbender" href="/" />
-          <LinkTab label="The Simpsons" href="/" />
+          <LinkTab label="Avatar: The Last Airbender" />
+          <LinkTab label="The Simpsons" />
         </Tabs>
       </AppBar>
     </div>
